962829a5ab08240ee1d3faa201cc4b37f68edd1a
Fausto Bresciani <fbsoftware@libero.it>
2025-09-28 17:42:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## FB-CODE
+ CSS standard FB
+ Libreria linkabile contenente i fogli di stile standard e  gli script javascipt standard da usare nei repository di "fbsoftware".
+ Attualmente contiene:
+
---
+++ js/tooltip.js
@@ -0,0 +1,11 @@
+// ------------------------------------------
+// 03/03/21	tooltip
+// ------------------------------------------
+  $( function() {
+    var tooltips = $( "[title]" ).tooltip({
+      position: {
+        my: "left top",
+        at: "right+5 top-5",
+        collision: "none"
+      }
+    });
